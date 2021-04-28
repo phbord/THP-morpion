6c12e5590b81b135c5a516971e17a246114af4e1
phbord <phbord@gmail.com>
2021-04-28 23:47:42 +0200

    création des fichiers et readme

--- app.rb
+++ app.rb
@@ -1,16 +1,12 @@
 require 'bundler'
 Bundler.require
 
-#require 'dotenv'
-Dotenv.load('.env')
-
 $:.unshift File.expand_path('./../lib', __FILE__)
-require 'views/index'
-#binding.pry
-
 require 'app/game'
 require 'app/player'
 require 'app/board'
 require 'app/board_case'
 require 'app/show'
 require 'app/application'
+#binding.pry
+
